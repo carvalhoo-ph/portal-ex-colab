1fce58acf2ecd48202a9744dc9b57bc0a4990900
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-01-17 13:31:28 -0300

    Commit 01

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portal-ex-colab');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portal-ex-colab TESTE 123456');
   });
 });
